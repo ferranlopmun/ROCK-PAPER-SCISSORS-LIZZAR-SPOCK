fcb15f1479983954edfc9d8ad5cb34906d81de04
ferranlopmun <101187295+ferranlopmun@users.noreply.github.com>
2023-04-26 20:30:43 +0200

    Delete ChoiceButton.js

--- ChoiceButton.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ChoiceButton = ({ choice, handleClick, children }) => {
-    return <button onClick={() => handleClick(choice)}>{children}</button>;
-};
-
-export default ChoiceButton;
